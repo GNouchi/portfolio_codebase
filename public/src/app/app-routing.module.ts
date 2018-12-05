6a3d25bb791d00746559f84f6b1fec0311b720a1
GNouchi <gnouchi7@gmail.com>
2018-12-05 13:23:27 -0800

    code preserve

---
+++ public/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { LandingComponent } from './landing/landing.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { CollaborateComponent } from './collaborate/collaborate.component';
+
+const routes: Routes = [
+
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'resume', component: ResumeComponent },
+  // { path: 'collaborate', component: CollaborateComponent },
+  { path: '', component: LandingComponent },
+  { path: '**',  pathMatch: 'full', redirectTo: '/' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
+
+
---
+++ public/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import 'materialize-css';
+import { MaterializeModule } from 'angular2-materialize';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { CollaborateComponent } from './collaborate/collaborate.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ResumeComponent,
+    ProjectsComponent,
+    LandingComponent,
+    CollaborateComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterializeModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
